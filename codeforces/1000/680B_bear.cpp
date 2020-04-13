7e1c6ee6a2a9340c7cf868f2a1f93d7c01c30beb
Jeff Kim <eunkeekim@gmail.com>
2020-04-13 13:33:27 -0400

    finished 680B

---
+++ codeforces/1000/680B_bear.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector> 
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, house;
+    cin >> n >> house; 
+    int input; 
+
+    vector<int> cities ;
+    for (int i = 0; i< n; i++) {
+        cin >> input; 
+        cities.push_back(input); 
+    }
+
+    int criminals = 0; 
+    if (cities[house-1] == 1) {
+        criminals ++; 
+    }
+
+    int left = house -2; 
+    int right = house; 
+
+    while (left >= 0 && right < n) {
+        if (cities[left] == cities[right] && cities[left] == 1) {
+            criminals +=2;
+        } 
+        left--;
+        right++; 
+    }
+    if (left == -1) {
+        while (right < n) {
+            if(cities[right] == 1) {
+                criminals ++; 
+            }
+            right++; 
+        }
+    } else if (right == n) {
+        while (left >= 0) {
+            if(cities[left] == 1) {
+                criminals ++; 
+            }
+            left --; 
+        }
+    } 
+    cout << criminals; 
+}
