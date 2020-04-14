d6b951f3146e58f0c964420ffef244e0b274ccc3
Jeff Kim <eunkeekim@gmail.com>
2020-04-14 11:32:39 -0400

    Finished questionA

---
+++ questionA.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>  // map and multimap
+#include <numeric>
+#include <set>            // set and multiset
+#include <unordered_map>  // unordered map/multimap
+#include <unordered_set>  //unordered set/multiset
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    cout << "hello " ; 
+}
