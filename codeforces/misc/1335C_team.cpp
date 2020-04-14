a8f9359ca5b7f7aae8f53aee01a272221921ceb9
Jeff Kim <eunkeekim@gmail.com>
2020-04-14 03:17:48 -0400

    finished 1335C_team composition

---
+++ codeforces/misc/1335C_team.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector> 
+#include <map> 
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t, n, maxTeam;
+    cin >> t; 
+    while (t > 0) {
+        cin >> n; 
+        maxTeam = 0; 
+
+        map <int, int> skillMap;
+        int skill; 
+        for (int i = 0; i < n; i++) {
+            cin >> skill; 
+            skillMap[skill]++; 
+            maxTeam = max(maxTeam, skillMap[skill]); 
+        }
+        int differentSkillTeam = skillMap.size() - 1; 
+        int sameSkillTeam = maxTeam; 
+        maxTeam = min(maxTeam, differentSkillTeam); 
+        if (sameSkillTeam - differentSkillTeam > 1) {
+            maxTeam ++; 
+        }
+        cout << maxTeam; 
+        t--;
+    }
+}
