3729e8768f95affeaf5d1f3e37749cdc3ad3dca3
Jeff Kim <eunkeekim@gmail.com>
2020-04-20 19:37:55 +0900

    added balanced brackets hackerrank

---
+++ hackerrank/balanced_brackets.cpp
@@ -0,0 +1,25 @@
+#include <stack>
+#include <string>
+
+using namespace std;
+
+string isBalanced(string s) {
+    if (s.empty()) {
+        return "YES"; 
+    }
+
+    stack <int> stack; 
+    for (char c: s) {
+        if (c == '{' || c == '[' || c == '(') {
+            stack.push(c); 
+        } else {
+            if (stack.empty()) return "NO"; 
+            if (c == ']' && stack.top() != '[') return "NO"; 
+            if (c == '}' && stack.top() != '{') return "NO"; 
+            if (c == ')' && stack.top() != '(') return "NO"; 
+            stack.pop(); 
+        }
+    }
+    if (stack.empty()) return "YES";
+    return "NO";
+}
