81b68c61b62254cc0b68a5fe3cf4f5fb86f567c7
Jeff Kim <eunkeekim@gmail.com>
2020-05-19 17:30:56 +0900

    added template file for cpp

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+     
+    cout << "Answer";
+}
+
