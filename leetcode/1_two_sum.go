a0393d47916d79ad836c30d50c243961c7b23590
Jeff Kim <eunkeekim@gmail.com>
2020-07-11 20:05:32 -0400

    added 1-two-sum in go

---
+++ leetcode/1_two_sum.go
@@ -0,0 +1,13 @@
+func twoSum(nums []int, target int) []int {
+	seen := make(map[int]int)
+
+	for i, num := range nums {
+		curr, exist := seen[target-num]
+		if exist {
+			return []int{i, curr}
+		}
+		seen[num] = i
+	}
+
+	return []int{0, 0}
+}
