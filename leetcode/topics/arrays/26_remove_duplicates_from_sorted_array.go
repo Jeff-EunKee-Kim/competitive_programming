e5f8e0584878ad5cfe952afa8c96115580f75b24
Jeff Kim <eunkeekim@gmail.com>
2020-08-02 01:10:54 -0400

    start leetcode by topics

---
+++ leetcode/topics/arrays/26_remove_duplicates_from_sorted_array.go
@@ -0,0 +1,14 @@
+func removeDuplicates(nums []int) int {
+	if len(nums) <= 1 {
+		return len(nums)
+	}
+
+	writeIdx := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			nums[writeIdx] = nums[i]
+			writeIdx++
+		}
+	}
+	return writeIdx
+}
