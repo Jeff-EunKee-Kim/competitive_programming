60c508cf3df6aa0385b809f26bdfd3e971d75036
Jeff Kim <eunkeekim@gmail.com>
2020-08-05 23:30:42 -0400

    add best time to buy sell stock 2

---
+++ leetcode/122_best_time_to_buy_and_sell_stock_2.go
@@ -0,0 +1,12 @@
+func maxProfit(prices []int) int {
+	total := 0
+	if len(prices) == 0 {
+		return total
+	}
+	for i := 0; i < len(prices)-1; i++ {
+		if prices[i] < prices[i+1] {
+			total += prices[i+1] - prices[i]
+		}
+	}
+	return total
+}
