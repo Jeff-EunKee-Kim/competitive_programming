6e14ca72dc7b72af60b3c9b8955b53ca5a3c5de2
Jeff Kim <eunkeekim@gmail.com>
2020-08-13 22:36:36 -0400

    add single number

---
+++ leetcode/topics/arrays/21_single_number.go
@@ -0,0 +1,25 @@
+func singleNumber(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	for i := 1; i < len(nums); i++ {
+		nums[0] = nums[0] ^ nums[i]
+	}
+	return nums[0]
+}
+
+// func singleNumber(nums []int) int {
+// 	numMap := make(map[int]bool)
+// 	for _, n := range nums {
+// 		if numMap[n] {
+// 			delete(numMap, n)
+// 		} else {
+// 			numMap[n] = true
+// 		}
+// 	}
+// 	for key, _ := range numMap {
+// 		return key
+// 	}
+// 	return -1
+// }
