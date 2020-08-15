e1936eaec50f9d56a8d059e4ea099b5147c20726
Jeff Kim <eunkeekim@gmail.com>
2020-08-15 01:18:22 -0400

    add intersection of two arrays

---
+++ leetcode/topics/arrays/350_intersection_of_two_arrays_II.go
@@ -0,0 +1,14 @@
+func intersect(nums1 []int, nums2 []int) []int {
+	map1 := make(map[int]int)
+	ans := make([]int, 0)
+	for _, num := range nums1 {
+		map1[num]++
+	}
+	for _, num := range nums2 {
+		if map1[num] > 0 {
+			ans = append(ans, num)
+			map1[num]--
+		}
+	}
+	return ans
+}
