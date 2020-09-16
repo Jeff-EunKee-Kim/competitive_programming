e68c9d435d530b065f4f9d49681604f69c8371ab
Jeff Kim <eunkeekim@gmail.com>
2020-09-16 00:37:34 -0400

    add first bad version

---
+++ leetcode/topics/sort/278_first_bad_version.go
@@ -0,0 +1,18 @@
+package sort
+
+func isBadVersion(version int) bool {
+	return true
+}
+
+func firstBadVersion(n int) int {
+	left, right := 0, n
+	for left < right {
+		mid := left + (right-left)/2
+		if isBadVersion(mid) {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
